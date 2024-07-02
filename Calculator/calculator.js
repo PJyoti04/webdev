81d83c3dc946f1d914a8f6a0fb0e3db158999307
PJyoti <bapunjyoti2004@gmail.com>
2024-07-02 23:57:12 +0530

    Calculator with functionalities

---
+++ Calculator/calculator.js
@@ -0,0 +1,30 @@
+function clearDisplay() {
+    document.getElementById('display').innerText = '0';
+}
+
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    if (display.innerText === '0') {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    if (display.innerText.length > 1) {
+        display.innerText = display.innerText.slice(0, -1);
+    } else {
+        display.innerText = '0';
+    }
+}

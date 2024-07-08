58ac4f3d3d0f2a738ad9a9e3be70f49b05f76795
PJyoti <bapunjyoti2004@gmail.com>
2024-07-08 18:17:28 +0530

    Responsive website with localstorage Implementation

--- Blog/script.js
+++ Blog/script.js
@@ -14,12 +14,14 @@ document.getElementById("log").addEventListener("click",() =>{
 document.getElementById("cross-login").addEventListener("click",() =>{
     document.getElementById("login").style.display="none";
     clearInputs();
+    document.getElementById("span1").style.display="none";
+    document.getElementById("span2").style.display="none";
     document.getElementById("content").style.display="block";
 })
 document.getElementById("cross-signin").addEventListener("click",() =>{
     document.getElementById("signin").style.display="none";
     clearInputs();
-    document.getElementById("content").style.display="block";
+    document.getElementById("span").style.display="none";    document.getElementById("content").style.display="block";
 })
 
 let login = [];
@@ -32,14 +34,43 @@ document.getElementById("btn").addEventListener("click",() => {
     if(name === "" || pass === ""){
         return;
     }else{
-        let obj={
-            username:name,
-            password:pass
+        document.getElementById("span2").style.display="none";
+        document.getElementById("span").style.display="none";
+        let i;
+        let j = 2;
+        for(i=0;i <= localStorage.length;i++){
+            // console.log(i);
+            if(localStorage.key(i) === `${name}`){
+                // console.log(localStorage.getItem(`${name}`));
+                if(localStorage.getItem((`${name}`)) == `${pass}`){
+                    j=0;
+                    break;
+                }else{
+                    j=1;
+                }
+                // document.getElementById("span").style.display="block";
+                
+            }
         }
-        login.push(obj);
-        console.log(login);
-        clearInputs();
-        document.getElementById("login").style.display="none";
+        if(j === 0){
+            let obj={
+                username:name,
+                password:pass
+            }
+            login.push(obj);
+            console.log(login);
+            clearInputs();
+            document.getElementById("login").style.display="none";
+            document.querySelector(".add").style.visibility = "visible";
+            document.getElementById("content").style.display="block";
+        }
+        else if(j === 1){
+            document.getElementById("span2").style.display="block"
+        }
+        else{
+            document.getElementById("span").style.display = "block";
+        }
+        
     }
 })
 
@@ -52,15 +83,36 @@ document.getElementById("button").addEventListener("click",() => {
     if(name === "" || pass === "" || email === ""){
         return;
     }else{
-        let obj={
-            username:name,
-            email:email,
-            password:pass
+        document.getElementById("span1").style.display="none";
+        let i;
+        let j;
+        for(i=0;i <= localStorage.length;i++){
+            // console.log(i);
+            if(localStorage.key(i) === `${name}`){
+                // document.getElementById("span1").style.display="block";
+                j=0;
+                break;
+            }else{
+                j=1;
+            }
         }
-        signin.push(obj);
-        console.log(signin);
-        clearInputs();
-        document.getElementById("signin").style.display="none";
+        if(j !== 0){
+            localStorage.setItem(`${name}`,`${pass}`);
+            let obj={
+                username:name,
+                email:email,
+                password:pass
+            }
+            signin.push(obj);
+            console.log(signin);
+            clearInputs();
+            document.getElementById("signin").style.display="none";
+            document.querySelector(".add").style.visibility = "visible";
+            document.getElementById("content").style.display="block";
+        }else{
+            document.getElementById("span1").style.display="block";
+        }
+        
     }
 })
 
@@ -78,7 +130,7 @@ document.getElementById("add-blog-cross").addEventListener("click",() =>{
     document.getElementById("content").style.display="block";
 })
 
-document.getElementById("txt").addEventListener("click",() =>{
+document.querySelector(".add").addEventListener("click",() =>{
     document.getElementById("add-blog").style.display="block";
     // clearInputs();
     document.getElementById("content").style.display="none";
@@ -116,7 +168,7 @@ function addBlog(title, image,i) {
     imgDiv.className = "img";
 
     let img = document.createElement("img");
-    img.src = image;
+    img.src= image;
     img.alt = "Blog Image";
 
     if(i % 2 === 0){
